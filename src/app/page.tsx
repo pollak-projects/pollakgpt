ec85244d06771064714317d23fd9834d90e0b8bd
Kedves1 <gombarmartin2006@gmail.com>
2025-05-14 09:41:20 +0200

    feat: gemini api test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { GoogleGenAI } from "@google/genai";
+const GEMINI_API_KEY = process.env.APIKEY;
 
-function Home() {
-  return <div>Home</div>;
+const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+
+async function Home() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash-lite",
+    contents: "Why is the sky blue?",
+    config: {
+      systemInstruction: "do not use markdown",
+    },
+  });
+
+  return <div className="">{response.text}</div>;
 }
 
 export default Home;
